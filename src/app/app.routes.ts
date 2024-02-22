c5d51cceae2700d3c4d253533fe8e1d8c283d61c
Christopher Tineo <christophertineo02@gmail.com>
2024-02-22 09:26:27 -0400

    create companies service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,21 @@
 import { Routes } from '@angular/router';
 
+/*
+
+Debe haber una pantalla de inicio
+Deben haber aseguradoras
+
+Hay 4 paginas:
+
+1. Inicio
+2. Guardados (Es un crud de las aseguradoras)
+  2.1 Te permite agregar, actualizar y eliminar una aseguradora
+  2.2 Para mostrar la lista de aseguradoras puedes usar un card con el nombre de la aseguradora, logo y una breve descripción.
+3. Mi poliza
+4. Ayuda
+
+*/
+
 export const routes: Routes = [
   {
     path: 'home',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
+import { IonicModule } from '@ionic/angular';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule],
 })
 export class HomePage {
   constructor() {}
